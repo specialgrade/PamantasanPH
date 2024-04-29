4a78818c20f76934ea86866919eb8c03f26d86dc
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-04-29 08:34:51 -0700

    set-up flask

--- run.py
+++ run.py
@@ -1,6 +1,6 @@
-from src import create_app
+from flask import Flask
 
-app = create_app()
+app = Flask(__name__)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/static/__init__.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-# initializing all the files under src to a python package
-def create_app():
-    app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'PamantasanPH'
-
-    return app
