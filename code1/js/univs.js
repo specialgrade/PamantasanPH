1ff2398ab6c8952019f0ccc526b1e0a84d4a7d70
Issa <aly.mariedelacruz@gmail.com>
2024-04-30 01:28:14 +0800

    Update files

--- code1/js/univs.js
+++ code1/js/univs.js
@@ -2,8 +2,10 @@
 const statusIcon = document.getElementById('status-icon');
 const statusText = document.getElementById('status-text');
 
-if (statusText.textContent === 'NOT YET OPENED') { 
+if (document.getElementById('status').textContent === 'NOT YET OPENED') {
 	statusIcon.classList.remove('open');
+	statusText.textContent = 'NOT YET OPENED';
 } else {
 	statusIcon.classList.add('open');
+	statusText.textContent = 'OPEN';
 }
