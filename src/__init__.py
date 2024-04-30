f38c070a32c543b2b1b922cf42ece5fcd07f9ce3
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-04-30 15:15:10 -0700

    added file

--- src/__init__.py
+++ src/__init__.py
@@ -5,4 +5,8 @@ def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'PamantasanPH'
 
+    from .routes import routes
+
+    app.register_blueprint(routes, url_prefix = '/')
+
     return app
