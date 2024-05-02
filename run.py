4d491f70f6de869f7e15025b46587b0c60f66140
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-05-02 08:48:07 -0700

    revisions 5.02

--- run.py
+++ run.py
@@ -1,1 +1,10 @@
-# hi
+from flask import Flask, render_template
+from static.app import create_app
+from static import routes
+
+app = create_app()
+
+routes.init_app(app)
+
+if __name__ == '__main__':
+    app.run(debug = True)
