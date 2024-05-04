cb0d384320cb3aecd70798a644741435ce66fb9b
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-05-04 16:30:58 -0700

    updated files

--- run.py
+++ run.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
 from src.app import create_app
 from src import routes
+from flask_mail import Mail
 
 app = create_app()
+
+mail = Mail(app)
 
 routes.init_app(app)
 
