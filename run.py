4a7e769bdb9d7bac76dc0befbc6f81e1e021968b
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-05-12 23:52:36 -0700

    updated files + added routes

--- run.py
+++ run.py
@@ -6,4 +6,4 @@ app = create_app()
 routes.init_app(app)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000, threaded=True, use_reloader=True)
+    app.run(debug=True)
--- src/routes.py
+++ src/routes.py
@@ -18,9 +18,13 @@ def init_app(app):
         return render_template('index.html')
     
     # routes to render the univs.html when they click the Universities button from the home page
-    @app.route('/home/universities', methods=['GET'])
+    @app.route('/home/universities')
     def universities():
         return render_template('univs.html')
+    
+    @app.route('/home/bookmarks')
+    def bookmarks():
+        return render_template('bookmarks.html')
     
     # route for sending email to the users
     @app.route('/newsletter', methods=['POST'])
@@ -40,3 +44,88 @@ def init_app(app):
             db.session.rollback()
             flash('You have already subscribed!', category='success')
             return redirect(url_for('index'))
+        
+    # routes for separate univs
+    @app.route('/home/universities/city-of-malabon-university', methods=['GET', 'POST'])
+    def CMU():
+        return render_template('CMU.html')
+    
+    @app.route('/home/universities/dr-filemon-c-aguilar-memorial-college-of-las-pinas', methods=['GET', 'POST'])
+    def Dr_Fil():
+        return render_template('dr.fil.html')
+    
+    @app.route('/home/universities/eulogio-"amang"-rodriguez-institute-of-science-&-technology', methods=['GET', 'POST'])
+    def Earist():
+        return render_template('earist.html')    
+    
+    @app.route('/home/universities/marikina-polytechnic-college', methods=['GET', 'POST'])
+    def MPC():
+        return render_template('mpc.html')
+    
+    @app.route('/home/universities/navotas-polytechnic-college', methods=['GET', 'POST'])
+    def NPC():
+        return render_template('npc.html')
+    
+    @app.route('/home/universities/paranaque-city-college', methods=['GET', 'POST'])
+    def PCC():
+        return render_template('pcc.html')
+    
+    @app.route('/home/universities/polytechnic-college-of-the-city-of Meycauayan', methods=['GET', 'POST'])
+    def PCCM():
+        return render_template('pccm.html')
+    
+    @app.route('/home/universities/pamantasan-ng-lungsod-ng-maynila', methods=['GET', 'POST'])
+    def PLM():
+        return render_template('plm.html')
+    
+    @app.route('/home/universities/pamantasan-ng-lungsod-ng-marikina', methods=['GET', 'POST'])
+    def PLMAR():
+        return render_template('plmar.html')
+    
+    @app.route('/home/universities/pamantasan-ng-lungsod-ng-muntinlupa', methods=['GET', 'POST'])
+    def PLMUN():
+        return render_template('plmun.html')
+    
+    @app.route('/home/universities/pamantasan-ng-lungsod-ng-pasig', methods=['GET', 'POST'])
+    def PLP():
+        return render_template('plp.html')
+    
+    @app.route('/home/universities/pamantasan-ng-lungsod-ng-valenzuela', methods=['GET', 'POST'])
+    def PLV():
+        return render_template('plv.html')
+    
+    @app.route('/home/universities/philippine-national-university', methods=['GET', 'POST'])
+    def PNU():
+        return render_template('pnu.html')
+    
+    @app.route('/home/universities/philippine-state-college-of-aeronautics', methods=['GET', 'POST'])
+    def PSCA():
+        return render_template('psca.html')
+    
+    @app.route('/home/universities/polytechnic-university-of-the-philippines', methods=['GET', 'POST'])
+    def PUP():
+        return render_template('pup.html')
+    
+    @app.route('/home/universities/quezon-city-university', methods=['GET', 'POST'])
+    def QCU():
+        return render_template('qcu.html')
+    
+    @app.route('/home/universities/taguig-city-university', methods=['GET', 'POST'])
+    def TCU():
+        return render_template('tcu.html')
+    
+    @app.route('/home/universities/technological-university-of-the-philippines', methods=['GET', 'POST'])
+    def TUP():
+        return render_template('tup.html')
+    
+    @app.route('/home/universities/universidad-de-manila', methods=['GET', 'POST'])
+    def UDM():
+        return render_template('udm.html')
+    
+    @app.route('/home/universities/university-of-makati', methods=['GET', 'POST'])
+    def UM():
+        return render_template('um.html')
+    
+    @app.route('/home/universities/university-of-the-philippines', methods=['GET', 'POST'])
+    def UP():
+        return render_template('up.html')
