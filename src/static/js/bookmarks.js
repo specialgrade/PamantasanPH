19d93d635659e0ab98e18f149445f5d8ff807b84
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-05-12 23:52:36 -0700

    updated files + added routes

--- src/static/js/bookmarks.js
+++ src/static/js/bookmarks.js
@@ -1,6 +1,6 @@
-// Bookmark function
 document.addEventListener("DOMContentLoaded", function() {
     var favsContainer = document.getElementById('favsContainer');
+    var placeholderText = document.getElementById('placeholderText');
 
     var bookmarkedContent = localStorage.getItem('bookmarkedContent');
     if (bookmarkedContent) {
@@ -8,35 +8,31 @@ document.addEventListener("DOMContentLoaded", function() {
         bookmarkedContent.forEach(function(content) {
             var div = document.createElement('div');
             div.innerHTML = content;
+            div.classList.add('bookmark-container'); // Adding a class for styling
 
-            var bookmarkIcon = div.querySelector('.bookmark-icon');  
-            bookmarkIcon.addEventListener('click', function() {
-                 
-                var confirmation = confirm("Click 'OK' to Remove from Bookmarks.");
-                if (confirmation) {
-                    favsContainer.removeChild(div);
+            var unbookmarkButton = document.createElement('button');
+            unbookmarkButton.textContent = 'Unbookmark';
+            unbookmarkButton.classList.add('unbookmark', 'button-style'); // Adding a class for button styling
 
-                    var index = bookmarkedContent.indexOf(content);
-                    if (index !== -1) {
-                        bookmarkedContent.splice(index, 1);
-                        localStorage.setItem('bookmarkedContent', JSON.stringify(bookmarkedContent));
-                    }
+            unbookmarkButton.addEventListener('click', function() {
+                favsContainer.removeChild(div);
+
+                var index = bookmarkedContent.indexOf(content);
+                if (index !== -1) {
+                    bookmarkedContent.splice(index, 1);
+                    localStorage.setItem('bookmarkedContent', JSON.stringify(bookmarkedContent));
+                }
+
+                if (favsContainer.children.length === 0) {
+                    placeholderText.style.display = 'block';
                 }
             });
 
+            div.appendChild(unbookmarkButton);
+
             favsContainer.appendChild(div);
         });
+    } else {
+        placeholderText.style.display = 'block';
     }
 });
-
-// Displayed text when the container is empty
-function checkEmptyContainer() {
-    var bookmarkedContent = localStorage.getItem('bookmarkedContent');
-    var favsContainer = document.getElementById('favsContainer');
-
-    if (!bookmarkedContent || JSON.parse(bookmarkedContent).length === 0) {
-        favsContainer.innerHTML = '<p class="bookmarks-text">Add your bookmarks here</p>';
-    } else {
-        favsContainer.innerHTML = ''; 
-    }
-}
