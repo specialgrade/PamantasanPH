c5ff837c84e1bc6474172c15bcd7da6f15d2981f
dmsimara <dmsimara@iskolarngbayan.pup.edu.ph>
2024-05-14 00:20:37 -0700

    update

--- src/app.py
+++ src/app.py
@@ -19,8 +19,8 @@ def create_app():
     app.config['MAIL_PORT'] = 465
     app.config['MAIL_USERNAME'] = '@gmail.com'
     app.config['MAIL_PASSWORD'] = ''
-    app.config['MAIL_USE_SSL'] = True
-    app.config['MAIL_USE_TLS'] = False
+    app.config['MAIL_USE_SSL'] = False
+    app.config['MAIL_USE_TLS'] = True
 
     # initializing the database and mail
     db.init_app(app)
