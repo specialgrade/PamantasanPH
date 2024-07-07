2c37fdf1b43bf841add894f0ab70c163a2a171c6
Daniella Simara <150517240+dmsimara@users.noreply.github.com>
2024-07-07 07:22:18 -0700

    Update forms.py

--- src/forms.py
+++ src/forms.py
@@ -47,24 +47,6 @@ class ResetPasswordForm(FlaskForm):
      confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
      password = PasswordField('Password', validators=[DataRequired()])
      submit = SubmitField('Reset Password')
-
-class UpdateAccountForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
-    email = StringField('Email', validators=[DataRequired(), Email()])
-    picture = FileField('Update Profile Picture', validators=[FileAllowed(['jpg', 'png'])])
-    submit = SubmitField('Update')
-
-    def validate_username(self, username):
-        if username.data != current_user.username:
-            user = User.query.filter_by(username=username.data).first()
-            if user:
-                raise ValidationError('That username is taken. Please choose another one.')
-        
-    def validate_email(self, email):
-        if email.data != current_user.email:
-            user = User.query.filter_by(email=email.data).first()
-            if user:
-                raise ValidationError('That email is taken. Please choose another one.')
             
 
 class VerifyOTPForm(FlaskForm):
